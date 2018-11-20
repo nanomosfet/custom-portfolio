346f37feb740a4f731c47507d9659081fe51c572
Nicolas D'Amico-Barbour <ndamicobarbour@gmail.com>
2018-11-19 23:31:55 -0800

    Css changes to add image button

--- src/containers/editable-image-item.js
+++ src/containers/editable-image-item.js
@@ -4,21 +4,24 @@ import { addItemImage } from './../actions'
 import './editable-image-item.css'
 const EditableImageItem = ({ dispatch, item }) => {
   return (
-    <div
-      className="w-100 rounded-top mx-auto"
-      style={{
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'contain',
-        height: '300px',
-        backgroundImage: `url('${item.source}')`
-      }}>
-      <label className={item.isAdded ? 'p bg-success image-upload image-added' : 'p bg-success image-upload'} >Add image
-        <input type="file" className="image-upload"
-          onChange={(event) => dispatch(addItemImage(item.id, event.target.files[0]))}
-        />
-      </label>
+    <div>
+      <div
+        className="w-100 rounded-top mx-auto"
+        style={{
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'contain',
+          height: '300px',
+          backgroundImage: `url('${item.source}')`
+        }}>
+        <label className={item.isAdded ? 'p bg-success image-upload image-added' : 'p bg-success image-upload'} >Add image
+          <input type="file" className="image-upload"
+            onChange={(event) => dispatch(addItemImage(item.id, event.target.files[0]))}
+          />
+        </label>
       </div>
+      <div className={item.isAdded ? 'image-margin added-margin' : 'image-margin'} />
+    </div>
   )
 }
 
