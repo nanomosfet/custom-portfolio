78e74643901a81f7e8a754ab14a544e76bfcc11f
Tim <timmyzsearcy@gmail.com>
2018-12-09 21:59:33 -0800

    no background color for images

--- src/components/style-map.js
+++ src/components/style-map.js
@@ -3,27 +3,27 @@ const baseBox = 'mt-1 px-1 pt-2 text-center h-100'
 const baseBoxMain = 'my-1 p-5  text-center h-100'
 const styleMap = {
   dark: {
-    image: `${baseImage} bg-light`,
+    image: `${baseImage}`,
     box: `${baseBox} text-white bg-dark`
   },
   light: {
-    image: `${baseImage} bg-dark`,
+    image: `${baseImage}`,
     box: `${baseBox} bg-light`
   },
   lightMain: {
-    image: `${baseImage} bg-dark`,
+    image: `${baseImage}`,
     box: `${baseBoxMain} bg-light`
   },
   darkMain: {
-    image: `${baseImage} bg-light`,
+    image: `${baseImage}`,
     box: `${baseBoxMain} text-white bg-dark`
   },
   undefined: {
-    image: `${baseImage} bg-dark`,
+    image: `${baseImage}`,
     box: `${baseBox} bg-light`
   },
   null: {
-    image: `${baseImage} bg-dark`,
+    image: `${baseImage} `,
     box: `${baseBox} bg-light`
   }
 }
